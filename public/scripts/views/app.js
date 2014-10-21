a6f44670adf981b3e56439629f6d428bb317f5d5
Nestor Alain <tchiss_a@epitech.eu>
2014-10-21 18:38:31 +0200

    vendor

--- public/scripts/views/app.js
+++ public/scripts/views/app.js
@@ -1,1 +1,18 @@
-define(["backbone"],function(){var e=Backbone.View.extend({initialize:function(){console.log("Wahoo!"),alert("yes")}});return e});
+define(["backbone"],function(){
+	var e=Backbone.View.extend({
+		initialize:function(){
+			console.log("Wahoo!");
+		}
+	});
+	return e;
+});
+
+/*define(["../routers"], function(Router){
+	var initialize: function(){
+		Router.initialize();
+	}
+
+	return {
+		initialize: initialize
+	};
+});*/
