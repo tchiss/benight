48f8b11145c4b1b64cadefb76e5ac551c7c9dcdc
kvacquier <Kylyu@MacBook-Pro-de-Kevin.local>
2015-02-25 22:48:44 +0100

    First Commit

---
+++ iOS/Benight/Benight/MyNavigationController.swift
@@ -0,0 +1,45 @@
+//
+//  MyNavigationController.swift
+//  Benight
+//
+//  Created by Kévin VACQUIER on 18/11/2014.
+//  Copyright (c) 2014 Benight. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: SideMenuNavigationController, SideMenuDelegate {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		sideMenu = SideMenu(sourceView: self.view, menuTableViewController: MyMenuTableViewController(), menuPosition:.Left)
+		sideMenu?.delegate = self
+		sideMenu?.menuWidth = 160.0
+		// make navigation bar showing over side menu
+		//view.bringSubviewToFront(navigationBar)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - SideMenu Delegate
+	func sideMenuWillOpen() {
+	}
+	
+	func sideMenuWillClose() {
+	}
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
+}
