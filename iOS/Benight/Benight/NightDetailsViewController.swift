d428c2388df3cbb3ee6134b1939d63f3b497e9c4
kvacquier <Kylyu@MacBook-Pro-de-Kevin.local>
2015-02-25 22:48:44 +0100

    First Commit

---
+++ iOS/Benight/Benight/NightDetailsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  NightDetailsViewController.swift
+//  Benight
+//
+//  Created by Kévin VACQUIER on 25/02/2015.
+//  Copyright (c) 2015 Benight. All rights reserved.
+//
+
+import UIKit
+
+class NightDetailsViewController: UIViewController {
+
+	var event: PFObject = PFObject(className: "Event")
+	
+	
+	@IBOutlet var switchInsc: UISwitch!
+	@IBOutlet var inscLabel: UILabel!
+	@IBOutlet var DateLabel: UILabel!
+	@IBOutlet var descLabel: UILabel!
+	@IBOutlet var nameLabel: UILabel!
+	
+	
+	@IBAction func switchChanger(sender: AnyObject) {
+		if (switchInsc.on)
+		{
+			inscLabel.text = "Inscrit"
+			var resa = PFObject(className:"Reservation")
+			resa["User"] = PFUser.currentUser()
+			resa["Event"] = event
+			resa.saveInBackground()
+		}
+		else
+		{
+			inscLabel.text = "Non Inscrit"
+			print("titi")
+
+		}
+
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		nameLabel.text = event["name"] as String!
+		//cell.PlaceLabel.text = events[indexPath.row]["location"] as String!
+		descLabel.text = event["theme"] as String!
+		DateLabel.text = (event["date"] as NSDate!).description
+	}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/Benight/Benight/NightsTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  NightsTableViewController.swift
+//  Benight
+//
+//  Created by Kévin VACQUIER on 28/01/2015.
+//  Copyright (c) 2015 Benight. All rights reserved.
+//
+
+import UIKit
+
+class NightsTableViewController: UITableViewController {
+	
+	var events: Array<AnyObject> = []
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		var query = PFQuery(className: "Event")
+		query.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
+			if (error != nil) {
+				NSLog("error " + error.localizedDescription)
+			}
+			else {
+				self.events = NSArray(array: objects)
+				self.tableView.reloadData()
+			}
+		})
+		self.tableView.backgroundColor = UIColor.blackColor()
+		// Uncomment the following line to preserve selection between presentations
+		// self.clearsSelectionOnViewWillAppear = false
+		
+		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+		// self.navigationItem.rightBarButtonItem = self.editButtonItem()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - Table view data source
+	
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		// #warning Potentially incomplete method implementation.
+		// Return the number of sections.
+		return 1
+	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// #warning Incomplete method implementation.
+		// Return the number of rows in the section.
+		return events.count
+	}
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("NightCell", forIndexPath: indexPath) as NightsTableViewCell
+
+		cell.fillCell(events[indexPath.row])
+		return cell
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+		if segue.identifier ==  "nightDetails"
+		{
+			let indexPath = self.tableView.indexPathForSelectedRow()!.row
+			let object = events[indexPath]
+			let vc = segue.destinationViewController as NightDetailsViewController
+			vc.event = object as PFObject
+		}
+	}
+	/*
+	// Override to support conditional editing of the table view.
+	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+	// Return NO if you do not want the specified item to be editable.
+	return true
+	}
+	*/
+	
+	/*
+	// Override to support editing the table view.
+	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+	if editingStyle == .Delete {
+	// Delete the row from the data source
+	tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+	} else if editingStyle == .Insert {
+	// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+	}
+	}
+	*/
+	
+	/*
+	// Override to support rearranging the table view.
+	override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+	
+	}
+	*/
+	
+	/*
+	// Override to support conditional rearranging of the table view.
+	override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+	// Return NO if you do not want the item to be re-orderable.
+	return true
+	}
+	*/
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	// Get the new view controller using [segue destinationViewController].
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
+}
