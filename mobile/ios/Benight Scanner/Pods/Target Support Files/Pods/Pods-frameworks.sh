bb0e9833bfd801a4ef5f7e82d39cf0eced6f3b2b
kvacquier <kvacquier@gmail.com>
2016-01-10 15:09:52 +0100

    Update

--- mobile/ios/Benight Scanner/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ mobile/ios/Benight Scanner/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -52,16 +52,20 @@ code_sign_if_enabled() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/Bolts.framework'
+  install_framework 'Pods/FBSDKCoreKit.framework'
+  install_framework 'Pods/FBSDKLoginKit.framework'
   install_framework 'Pods/Parse.framework'
-  install_framework 'Pods/ParseUI.framework'
   install_framework 'Pods/QRCodeReader.framework'
   install_framework 'Pods/SwiftSpinner.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/Bolts.framework'
+  install_framework 'Pods/FBSDKCoreKit.framework'
+  install_framework 'Pods/FBSDKLoginKit.framework'
   install_framework 'Pods/Parse.framework'
-  install_framework 'Pods/ParseUI.framework'
   install_framework 'Pods/QRCodeReader.framework'
   install_framework 'Pods/SwiftSpinner.framework'
 fi
