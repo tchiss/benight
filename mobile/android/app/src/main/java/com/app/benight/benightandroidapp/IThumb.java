c745a67548987dc63d488a42cbe89732a4285a1b
Jeirgino <jeirgino@gmail.com>
2016-01-15 14:42:44 +0100

    New commit
    Repackaging all the android app

--- mobile/android/app/src/main/java/com/app/benight/benightandroidapp/IThumb.java
+++
@@ -1,7 +0,0 @@
-package com.app.benight.benightandroidapp;
-
-import com.app.benight.benightandroidapp.GalleryPhotoActivity.UIHandler;
-
-public interface IThumb {
-	public void onSaveThumbURL(UIHandler uih, ImageContener ic);
-}
