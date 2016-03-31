1b48c06c7393159417feb163d055fc2f1195d99e
Guillaume <guillaume2.martin@epitech.eu>
2016-03-31 14:57:57 +0200

    commit 31/03

---
+++ mobile/windowsPhone/Benight_application/Benight_application/Registration.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Xml;
+using System.IO.IsolatedStorage;
+using System.Xml.Serialization;
+using System.IO;
+using Parse;
+
+namespace Benight_application
+{
+    public partial class Registration : PhoneApplicationPage
+    {
+        public Registration()
+        {
+            InitializeComponent();
+            textBox1.Text = "";
+            Password1.Password = "";
+            Password2.Password = "";
+            textBox3.Text = "";
+        }
+
+        public void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var user = new ParseUser()
+            {
+                Username = "",
+                Password = "",
+                Email = ""
+            };
+
+            try
+            {
+                if (textBox1.Text != "" &&
+                    Password1.Password.ToString() != "" &&
+                    Password2.Password.ToString() != "" &&
+                    textBox3.Text != "")
+                {
+                    if (Password1.Password == Password2.Password)
+                    {
+                        user.Username = textBox1.Text;
+                        user.Password = Password1.Password.ToString();
+                        user.Email = textBox3.Text;
+
+                        user.SignUpAsync();
+
+                        NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                        MessageBox.Show("Vérifiez votre boite mail, un lien de confirmation vous a été envoyé.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Le mot de passe n'est pas correctement écrit.");
+                    }
+                }
+                else
+                {
+                    if (textBox1.Text == "")
+                        MessageBox.Show("Veuillez saisir un Pseudo.");
+                    if (Password1.Password.ToString() == "")
+                        MessageBox.Show("Veuillez saisir un Mot de passe.");
+                    if (textBox3.Text == "")
+                        MessageBox.Show("Veuillez saisir un eMail.");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Message erreur suivant: " + exc);
+            }
+        }
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ mobile/windowsPhone/Benight_application/Benight_application/ToDoEv.cs
@@ -0,0 +1,135 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benight_application
+{
+    public class TodoEv : INotifyPropertyChanged
+    {
+        public static readonly string ClassName = "Event";
+
+        public TodoEv() : this(new ParseObject(ClassName)) { }
+        public TodoEv(ParseObject backingObject)
+        {
+            if (backingObject.ClassName != ClassName)
+            {
+                throw new ArgumentException("Must create TodoItems with the proper ClassName");
+            }
+            //this.BackingObject = backingObject;
+            BackingObject = backingObject;
+        }
+
+        static public string myEvName(string name)
+        {
+            return (name);
+        }
+
+
+
+        static public ParseObject BackingObject { get; private set; }
+
+        public bool IsDirty
+        {
+            get
+            {
+                return BackingObject.IsDirty;
+            }
+        }
+
+        static public string name
+        {
+            get
+            {
+                return BackingObject.ContainsKey("name") ? BackingObject.Get<string>("name") : null;
+
+            }
+            set
+            {
+                if (value != name)
+                {
+                    BackingObject["name"] = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        static public DateTime date
+        {
+            get
+            {
+                return BackingObject.ContainsKey("date") ? BackingObject.Get<DateTime>("date") : date;
+
+            }
+            set
+            {
+                if (value != date)
+                {
+                    BackingObject["date"] = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        static public string objectId
+        {
+            get
+            {
+                return BackingObject.ContainsKey("objectId") ? BackingObject.Get<string>("objectId") : null;
+
+            }
+            set
+            {
+                if (value != objectId)
+                {
+                    BackingObject["objectId"] = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return BackingObject.ContainsKey("Description") ? BackingObject.Get<string>("Description") : null;
+            }
+            set
+            {
+                if (value != Description)
+                {
+                    BackingObject["Description"] = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var del = PropertyChanged;
+            if (del != null)
+            {
+                del(this, new PropertyChangedEventArgs(propertyName));
+                del(this, new PropertyChangedEventArgs("IsDirty"));
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await BackingObject.SaveAsync();
+            OnPropertyChanged("IsDirty");
+        }
+
+        public void Revert()
+        {
+            BackingObject.Revert();
+            OnPropertyChanged(String.Empty);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
